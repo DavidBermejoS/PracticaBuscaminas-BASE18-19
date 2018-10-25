9334a1fec8e1f8003452431c660b12cd091382cd
David <dbermejosimon@gmail.com>
2018-10-25 17:32:50 +0200

    Clase PaintTime
    la clase hereda de Thread, será un hilo que se encargue siempre que no acabe el juego de llamar al metodo de ventana principal que imprime el tiempo

---
+++ src/PaintTime.java
@@ -0,0 +1,25 @@
+/**
+ * @author David Bermejo Simon
+ **/
+public class PaintTime extends Thread {
+    VentanaPrincipal ventana;
+
+    public PaintTime(VentanaPrincipal ventana) {
+        this.ventana = ventana;
+    }
+
+
+    @Override
+    public void run() {
+        while (!ventana.getJuego().esFinJuego()) {
+            try {
+                sleep(1000);
+                ventana.actualizarTiempo(ventana.getJuego().getTiempo());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
