7bbd6445e5d2347e5abc7d22e95d867f4317b2d3
David Bermejo <dbermejosimon@gmail.com>
2018-10-27 21:19:02 +0200

    añadida funcionalidad score
    se ha añadido la funcionalidad de guardar en un fichero de texto las puntuaciones de todos los usuarios que quieran guardar su puntuación.
    Además se ha agregado la funcionalidad de mostrar un panel para cerrar el juego cuando este ha finalizado

---
+++ src/WriteScore.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WriteScore {
+    VentanaPrincipal ventana;
+    File fich;
+
+    public WriteScore(VentanaPrincipal ventana){
+        this.ventana = ventana;
+        this.fich = new File("Score/high_score");
+    }
+
+    public void writeFile(String user){
+        try(PrintWriter pw = new PrintWriter(new FileWriter(fich,true))){
+            pw.println("User: "+user+" ---> Score: "+ventana.pantallaPuntuacion.getText()+" ---> Tiempo: "+ventana.labelTiempo.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
